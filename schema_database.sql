96c6f59862f6814da0e48f1f94fac9e6ecb4684e
João Victor Cordeiro <58659032+JoaoCordeir0@users.noreply.github.com>
2023-04-08 15:53:21 -0300

    Update schema_database.sql

--- schema_database.sql
+++ schema_database.sql
@@ -3,14 +3,14 @@ use iclinic;
 
 create table funcionario(	
     idFuncionario int primary key auto_increment,
-	codFuncionario varchar(50),
+    codFuncionario varchar(50),
     nomeFuncionario varchar(255),	
     nomeCargo varchar(255),
     dataInsercao date default now()
 );
 
-create table turnos(
-	idTurno int primary key auto_increment,
+create table turnos( 
+    idTurno int primary key auto_increment,
     diaTurno date,
     tipoTurno varchar(255),
     idFuncionario int,
